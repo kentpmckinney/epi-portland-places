65d2272645ba9fabf70cffdced02fa278c95a16e
wrunion <winterrunion@gmail.com>
2020-03-17 18:57:47 -0700

    Add webpack template files.

---
+++ src/main.js
@@ -0,0 +1,12 @@
+/* eslint-disable indent */
+/* eslint-disable no-unused-vars */
+import $ from 'jquery';
+import './style.css';
+
+$(document).ready(function() {
+
+  // $('').click(function() {
+    
+  // });
+  
+});
